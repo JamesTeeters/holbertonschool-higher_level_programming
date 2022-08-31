b27028f0a51c6847e8d271d4f36e4d8a1da5b19e
JamesTeeters <4925@holbertonschool.com>
2022-08-31 14:02:43 -0700

    ''

---
+++ 0x01-python-import_modules/1-args.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    num = len(argv) - 1
+    if num == 0:
+        punc = '.'
+    else:
+        punc = ':'
+    if num == 1:
+        arg = "argument"
+    else:
+        arg = "arguments"
+    
+    i = 1
+    print("{} {}{}".format(num, arg, punc))
+    for i in range argv[1:]:
+        print ("{}: {}".foramt(count, arrgv[i]))
+        i += 1 
