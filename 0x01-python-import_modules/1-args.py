44b8ff7cd646e84f71b4c58dc7179961bd0b64d5
JamesTeeters <4925@holbertonschool.com>
2022-08-31 14:23:16 -0700

    'finally fixed 'Hopefully''

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -3,16 +3,14 @@ if __name__ == "__main__":
     from sys import argv
     num = len(argv) - 1
     if num == 0:
-        punc = '.'
-    else:
-        punc = ':'
+        print("0 arguments.")
+        exit()
     if num == 1:
-        arg = "argument"
+        arg = 'argument'
     else:
-        arg = "arguments"
-    
-    i = 1
-    print("{} {}{}".format(num, arg, punc), end='')
+        arg = 'arguments'
+    print("{} {}:".format(num, arg))
+    c = 1
     for i in argv[1:]:
-        print ("{}: {}".format(i, argv[i]))
-        i += 1 
+        print("{}: {}".format(c, argv[c]))
+        c += 1
