3ff18d1f3399c1a0076cc3760244997235916a72
JamesTeeters <4925@holbertonschool.com>
2022-09-01 11:16:06 -0700

    'typo'

--- 0x02-python-data_structures/7-add_tuple.py
+++ 0x02-python-data_structures/7-add_tuple.py
@@ -9,6 +9,6 @@ def add_tuple(tuple_a=(), tuple_b=()):
     if len2 < 2:
         while len2 < 2:
             tuple_b += (0,)
-            len 2 += 1
+            len2 += 1
     tuple_c = ((tuple_a[0] + tuple_b[0]), (tuple_a[1] = tuple_b[1]))
     return tuple_c
