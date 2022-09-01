dbecbbd09ace3df253c79449f9b6747be18ddc9c
JamesTeeters <4925@holbertonschool.com>
2022-09-01 11:33:09 -0700

    ''

--- 0x02-python-data_structures/10-divisible_by_2.py
+++ 0x02-python-data_structures/10-divisible_by_2.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    case = [true if i % 2 == 0 else false for i in my_list]
-    return case
+    if my_list:
+        case = [True if i % 2 == 0 else False for i in my_list]
+        return case
+    else:
+        return None
