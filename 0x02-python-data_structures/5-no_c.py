1b96757122c67f31bba67a415247ae6d297ad1c7
JamesTeeters <4925@holbertonschool.com>
2022-09-01 10:50:39 -0700

    'remove 'c' and 'C' from string'

--- 0x02-python-data_structures/5-no_c.py
+++ 0x02-python-data_structures/5-no_c.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def no_c(my_string):
+    my_list = list(my_string)
+    for i in my_list:
+        while 'c' in my_list:
+            my_list.remove('c')
+        while 'C' in my_list:
+            my_list.remove('C')
+        my_string = ''.join(str(i) for i in my_list)
+        return my_string
