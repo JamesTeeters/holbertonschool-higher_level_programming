ca790f0bbd25ec1f3edf7dfeebdfd92b75b28e3f
JamesTeeters <4925@holbertonschool.com>
2022-09-01 10:22:01 -0700

    'retrive value of index'

--- 0x02-python-data_structures/1-element_at.py
+++ 0x02-python-data_structures/1-element_at.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    for i in my_list:
-        if i == idx:
-            print({}.format(i))
+    if idx < 0 or idx >= len(mylist):
+        return(None)
+    return(my_list[idx])
