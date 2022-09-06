ef56cc9c6279971492dc4d9059f36c8cdbe0a70c
JamesTeeters <4925@holbertonschool.com>
2022-09-06 15:30:28 -0700

    'fixed pep8 syntax'

--- 0x03-python-more_data_structures/0-square_matrix_simple.py
+++ 0x03-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     matrix_cp = []
-    for i in range (len(matrix)):
+    for i in range(len(matrix)):
         row = map(lambda n: n * n, matrix[i])
         matrix_cp.append(list(row))
     return matrix_cp
