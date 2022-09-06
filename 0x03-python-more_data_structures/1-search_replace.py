b237550f4169d65efdd325ed15137c4a9c74d0f0
JamesTeeters <4925@holbertonstudents.com>
2022-09-06 17:27:07 -0500

    'replace index of list'

--- 0x03-python-more_data_structures/1-search_replace.py
+++ 0x03-python-more_data_structures/1-search_replace.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    list_cp = my_list.copy()
+    for i in range(len(list_cp)):
+        if list_cp[i] == search:
+            list_cp[i] = replace
+        return list_cp
