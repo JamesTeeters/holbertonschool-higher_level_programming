f0afbe9a29d5d7e3b0f26ccc9cef4826465f4f71
JamesTeeters <4925@holbertonstudents.com>
2022-09-06 16:55:33 -0500

    'square matrix'

--- 0x03-python-more_data_structures/0-square_matrix_simple.py
+++ 0x03-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    print(matrix)
+    matrix_cp = []
+    for i in range (len(matrix)):
+        row = map(lambda n: n * n, matrix[i])
+        matrix_cp.append(list(row))
+    return matrix_cp
