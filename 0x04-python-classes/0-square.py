0ccf1ed6b8d097ce6893b9b89719e96bd750b7af
JamesTeeters <4925@holbertonstudents.com>
2022-09-07 04:03:22 -0500

    'added new line at end of file'

--- 0x04-python-classes/0-square.py
+++ 0x04-python-classes/0-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """A class Square that defines a square by a size"""
 class Square:
-    """creating Squar class"""
+    """Created Square class"""
     def __init__(self, size):
-        """initilizing Square size"""
+        """initilizing attributes"""
         self.__size = size
