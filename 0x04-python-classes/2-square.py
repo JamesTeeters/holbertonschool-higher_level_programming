e9bc5c7c3de208f80886e01748b0f7712f41fc30
JamesTeeters <4925@holbertonstudents.com>
2022-09-07 04:13:32 -0500

    'added comments fixed method location'

--- 0x04-python-classes/2-square.py
+++ 0x04-python-classes/2-square.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
+"""added method area"""
 class Square:
+    """Created class Square"""
     def __init__(self, size=0):
+        """Assign atrributes to Square"""
         self.__size = size
         if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        def area(self):
-            return size * size
+    def area(self):
+        """Assign methods to Square"""
+         return self.__size * self.__size
