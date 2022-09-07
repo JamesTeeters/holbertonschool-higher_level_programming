862f938d88eae41857aca5b8df6e540fe0c9b086
JamesTeeters <4925@holbertonstudents.com>
2022-09-07 03:49:54 -0500

    'added commits'

--- 0x04-python-classes/0-square.py
+++ 0x04-python-classes/0-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""A class Square that defines a square by a size"""
 class Square:
+    """creating Squar class"""
     def __init__(self, size):
+        """initilizing Square size"""
         self.__size = size
