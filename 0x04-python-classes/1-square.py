787ace78b3684bf2bb0dc54387bce4359a0b8dcb
JamesTeeters <4925@holbertonstudents.com>
2022-09-07 03:27:02 -0500

    'raised errors'

--- 0x04-python-classes/1-square.py
+++ 0x04-python-classes/1-square.py
@@ -2,3 +2,7 @@
 class Square:
     def __init__(self, size=0):
         self.__size = size
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
