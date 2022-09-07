affb04a7eb3663e30c0214dc0a4320f276adf288
JamesTeeters <4925@holbertonstudents.com>
2022-09-07 04:44:01 -0500

    'print square'

---
+++ 0x04-python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""added method area"""
+class Square:
+    """Created class Square"""
+    def __init__(self, size=0):
+        """Assign atrributes to Square"""
+        self.__size = size
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+    def area(self):
+        return self.__size * self.__size
+    @property
+
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value 
+        if type(value) != int:
+            raise TypeError("size must be integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+    def my_print(self):
+        for i in range(self.__size):
+            for i in range(self.__size):
+                print("#", end=' ')
+            print()
