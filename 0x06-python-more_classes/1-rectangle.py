76d975bbb78ce9df9d8ddf97969df4e9cc4ddd8b
JamesTeeters <4925@holbertonstudents.com>
2022-09-13 09:32:57 -0500

    'add area and perimeter'

--- 0x06-python-more_classes/1-rectangle.py
+++ 0x06-python-more_classes/1-rectangle.py
@@ -1,1 +1,53 @@
 #!/usr/bin/python3
+"""
+Make a Rectangle
+"""
+
+
+class Rectangle:
+    """Created Rectangle Class"""
+    def __init__(self, width=0, height=0):
+        """set attributes of rectangle"""
+        self.__width = width
+        if type(width) != int:
+            raise TypeError("width must be an integer")
+        elif width < 0:
+            raise ValueError("width must be >= 0")
+        self.__height = height
+        if type(height) != int:
+            raise TypeError("height must be an integer")
+        elif height < 0:
+            raise ValueError("height must be >= 0")
+
+    def area(self):
+        return self.__height * self.__width
+
+    def perimeter(self):
+        if self.__width <= 0 or self.__height <= 0:
+            return 0
+        else:
+            return (self.__width + self.__height) * 2
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
