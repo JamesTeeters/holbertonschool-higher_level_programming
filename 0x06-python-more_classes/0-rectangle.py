63bf964c3270ca1cf693b8f1eaae37e3f26e9a73
JamesTeeters <4925@holbertonstudents.com>
2022-09-13 09:15:11 -0500

    'typo'

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -15,7 +15,7 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         self.__height = height
         if type(height) != int:
-            raise TypeError("Height must be an integer")
+            raise TypeError("height must be an integer")
         elif height < 0:
             raise ValueError("height must be >= 0")
 
@@ -38,7 +38,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         if type(value) != int:
-            raise TypeError("Height must be an integer")
+            raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
