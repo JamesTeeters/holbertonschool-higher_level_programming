60e609fff5b9ba2ee5132b678432cd38ec1fede4
JamesTeeters <4925@holbertonstudents.com>
2022-09-15 16:36:50 -0500

    'added area'

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""inheritance"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """subclass of BaseGeometry"""
+    def __init__(self, width, height):
+        """attributes of rectangle"""
+        if super().integer_validator("width", width):
+            self.width = width
+        if super().integer_validator("height", height):
+            self.height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
