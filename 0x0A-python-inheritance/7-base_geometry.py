f1bf49d15d06269d23259edd9542958d46d9935e
JamesTeeters <4925@holbertonstudents.com>
2022-09-15 15:13:35 -0500

    'added integer validator'

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""inheritance"""
+
+
+class BaseGeometry:
+    """created empty class"""
+    def area(self):
+        """add attributes to class"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        self.name = name
+        self.value = value
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise TypeError("{} must be greater than 0".format(name))
