a8a0d88c048a5f6cfd1841d3734424bef34c6f9d
JamesTeeters <4925@holbertonstudents.com>
2022-09-15 14:49:16 -0500

    'fixed pep8'

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """inheritance"""
+
+
 def is_kind_of_class(obj, a_class):
     """return true if is instance"""
-    return isinstance (obj, a_class)
+    return isinstance(obj, a_class)
