b53fadd889be9d7762fad48b1894efb9ebca4b3c
JamesTeeters <4925@holbertonstudents.com>
2022-09-15 16:17:08 -0500

    'fixed pep8'

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,16 +2,23 @@
 """inheritance"""
 
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+class BaseGeometry:
+    """created empty class"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise TypeError("{} must be greater than 0".format(name))
+
 
 class Rectangle(BaseGeometry):
     """subclass of BaseGeometry"""
-
     def __init__(self, width, height):
         """attributes of rectangle"""
-
         if super().integer_validator("width", width):
             self.__width = width
-
         if super().integer_validator("height", height):
             self.__height = height
