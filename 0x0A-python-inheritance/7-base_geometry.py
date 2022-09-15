64325ae7f125f1b2b986c70d3ebd9f0995b8214d
JamesTeeters <4925@holbertonstudents.com>
2022-09-15 16:14:23 -0500

    'made a rectange, understanding subclasses'

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -4,13 +4,11 @@
 
 class BaseGeometry:
     """created empty class"""
+    
     def area(self):
-        """add attributes to class"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        self.name = name
-        self.value = value
         if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""inheritance"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+class Rectangle(BaseGeometry):
+    """subclass of BaseGeometry"""
+
+    def __init__(self, width, height):
+        """attributes of rectangle"""
+
+        if super().integer_validator("width", width):
+            self.__width = width
+
+        if super().integer_validator("height", height):
+            self.__height = height
