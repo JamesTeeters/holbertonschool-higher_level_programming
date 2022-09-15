449850962955a42b5158484f3e02af0e6fc448dd
JamesTeeters <4925@holbertonstudents.com>
2022-09-15 15:03:05 -0500

    'Made empty area method'

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""inheritance"""
+
+
+class BaseGeometry:
+    """created empty class"""
+    def area(self):
+        """add attributes to class"""
+        raise Exception("area() is not implemented")
