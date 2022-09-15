c94e58cc992a0866500b0168674067b7eb8ef004
JamesTeeters <4925@holbertonstudents.com>
2022-09-15 14:57:39 -0500

    'issubclass()'

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+"""inheritance"""
+
+
+def inherits_from(obj, a_class):
+    """Return true if inherited from class"""
+    return issubclass(type(obj), a_class) and type(obj) != a_class
