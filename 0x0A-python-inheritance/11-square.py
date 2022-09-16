62fb79dda075a1a38a1d393e4b0072f65e22d0fc
JamesTeeters <4925@holbertonstudents.com>
2022-09-16 13:16:43 -0500

    ''

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,1 +1,46 @@
 #!/usr/bin/python3
+"""inheritance"""
+
+
+class BaseGeometry:
+    """created empty class"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """subclass of BaseGeometry"""
+    def __init__(self, width, height):
+        """attributes of rectangle"""
+        self.integer_validator("width", width)
+        self.width = width
+        self.integer_validator("height", height)
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.width, self.height)
+
+
+class Square(Rectangle):
+    """Subclass of Rectangle"""
+    def __init__(self, size):
+        """attributes of square"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.size = size
+
+    def area(self):
+        return self.size * self.size
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.size, self.size)
