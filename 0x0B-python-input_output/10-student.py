369be2347fe8c9cc703b260bbc82bf9cc8faaab6
JamesTeeters <4925@holbertonstudents.com>
2022-09-19 14:54:40 -0500

    'added attrs var'

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""Input Output"""
+
+
+class Student:
+    """make a student"""
+    def __init__(self, first_name, last_name, age):
+        """add attributes to student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        self.attrs = attrs
+        if attrs == None:
+            return self.__dict__
+        attr_list = {}
+        for i in attrs:
+            if i in self.__dict__:
+                attr_list[i] = self.__dict__
+        return attr_list
