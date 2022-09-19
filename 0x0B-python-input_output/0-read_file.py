03c45948c0e5b597d91a938e3fa98f18c07b683f
JamesTeeters <4925@holbertonstudents.com>
2022-09-19 11:50:49 -0500

    'fixed pep8'

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Input Output"""
+
+
 def read_file(filename=""):
-    with open (filename, 'r') as fi:
-        for line in fi:
-            print (line, end='')
+    with open(filename, 'r') as f:
+        for line in f:
+            print(line, end='')
