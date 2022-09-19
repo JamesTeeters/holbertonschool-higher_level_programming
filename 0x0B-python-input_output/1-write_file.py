e5e38727b89abdd40bda99a0b4bad9a290bae04d
JamesTeeters <4925@holbertonstudents.com>
2022-09-19 12:02:10 -0500

    'write text in file'

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""Input Output"""
+
+
+from re import T
+
+
+def write_file(filename="", text=""):
+    """Write in Text file"""
+    with open(filename, 'w') as f:
+        f.write(text)
+    return len(text)
