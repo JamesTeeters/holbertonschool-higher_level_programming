8ec0ad4df8f68198615a1fc1680757beac8524c1
JamesTeeters <4925@holbertonstudents.com>
2022-09-19 13:26:37 -0500

    'did it work'

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""Input Output"""
+
+import json
+
+
+def load_from_json_file(filename):
+    """create object from json"""
+    with open(filename, 'w') as f:
+        json.dump(filename, f)
