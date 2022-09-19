3446834149c9a1970b194b1c0ee9931ad59aae2f
JamesTeeters <4925@holbertonstudents.com>
2022-09-19 14:19:46 -0500

    'class into json'

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""Input Output"""
+import sys
+import json
+
+
+load = __import__('6-load_from_json_file').load_from_json_file
+save = __import__('5-save_to_json_file').save_to_json_file
--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""Input Output"""
+
+import json
+
+
+def class_to_json(obj):
+    """dictionary description for JSON serialization of an object:"""
+    return json.dumps(obj.__dict__)
