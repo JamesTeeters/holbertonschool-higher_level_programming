175e6c737c73c5f416549cac6873f34291bcf031
JamesTeeters <4925@holbertonstudents.com>
2022-09-19 13:07:18 -0500

    'save json in text file'

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""Input Output"""
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """write from jason to python"""
+    with open(filename, 'w') as f:
+        f.write(json.dumps(my_obj))
