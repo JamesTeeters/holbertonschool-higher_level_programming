23a00d58c7067a072e551ba920d910f7f9a99c45
JamesTeeters <4925@holbertonstudents.com>
2022-09-19 15:07:27 -0500

    'i don't know anymore'

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""Input Output"""
+
+
+Student = __import__('10-student').Student
+def reload_from_json(self, json):
+    self.json = json
+    for i in json:
+        setattr(self, i, json[i])
