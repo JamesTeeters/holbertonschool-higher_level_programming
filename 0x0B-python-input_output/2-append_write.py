e94b0a0ad854722355057a1aaaedf179386eb217
JamesTeeters <4925@holbertonstudents.com>
2022-09-19 12:09:38 -0500

    'append text to file'

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""Input Output"""
+
+
+def append_write(filename="", text=""):
+    """append text to file"""
+    with open(filename, 'a') as f:
+        f.write(text)
+    return len(text)
