38e22d299e252cf5d05a44068bca972a18d263b2
JamesTeeters <4925@holbertonstudents.com>
2022-09-19 14:24:26 -0500

    'removed json'

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -2,9 +2,6 @@
 """Input Output"""
 
 
-import json
-
-
 def class_to_json(obj):
     """dictionary description for JSON serialization of an object:"""
-    return json.dumps(obj.__dict__)
+    return (obj.__dict__)
