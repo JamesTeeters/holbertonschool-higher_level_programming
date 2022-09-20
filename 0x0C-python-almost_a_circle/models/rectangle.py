4a49c6c86663972a67dc6e1dd795d5c893799cb6
JamesTeeters <4925@holbertonstudents.com>
2022-09-20 17:23:49 -0500

    'made a rectangle Class with (width, height, x, and y) attributes'

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+"""Almost a Circle"""
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Made sub class of Base called Rectangle"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Attributes of Rectangle"""
+        super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @property
+    def x(self):
+        return self.__x
+
+    @property
+    def y(self):
+        return self.__y
+
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    @x.setter
+    def x(self, value):
+        if type(value) != int:
+            raise TypeError("x must be an integer")
+        elif value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @y.setter
+    def y(self, value):
+        if type(value) != int:
+            raise TypeError("y must be an integer")
+        elif value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
