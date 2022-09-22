1ee6fa2e21bc66948387fd15011a3bf4b4d96392
JamesTeeters <4925@holbertonstudents.com>
2022-09-22 18:53:37 -0500

    'checked if file exists'

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Almost a Circle"""
 import json
+from os.path import exists
 
 
 class Base:
@@ -57,7 +58,7 @@ class Base:
         """return a list of all instances of subclasses"""
         filename = cls.__name__ + ".json"
         json_list = []
-        if not filename:
+        if  not exists(filename):
             return []
         else:
             with open(filename, 'r') as f:
