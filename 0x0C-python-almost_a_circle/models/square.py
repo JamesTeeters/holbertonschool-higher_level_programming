7f929c9f6df2dfcf1fde7ede750493dfc15a2925
JamesTeeters <4925@holbertonstudents.com>
2022-09-22 11:03:12 -0500

    'made Square class'

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Almost a circle"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Created a subclass of rectangle square"""
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+        self.size = size
