cbb153c02f2463cf5c39b0332a980e3057f162c9
JamesTeeters <4925@holbertonstudents.com>
2022-09-22 14:30:41 -0500

    'addded static method'

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -16,9 +16,10 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """convert to json"""
-        if list_dictionaries is None or list_dictionaries == []:
+        if list_dictionaries is None:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
