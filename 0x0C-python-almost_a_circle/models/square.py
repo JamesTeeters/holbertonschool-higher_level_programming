879ca71082890d94c4484b093b128324a6473d34
JamesTeeters <4925@holbertonstudents.com>
2022-09-22 11:13:36 -0500

    'added size setter'

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,3 +9,16 @@ class Square(Rectangle):
         """add attribute to Square"""
         super().__init__(size, size, x, y, id)
         self.size = size
+
+    @property
+    def size(self):
+        return self.height
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be > 0")
+        self.__width = value
+        self.__heigth = value
