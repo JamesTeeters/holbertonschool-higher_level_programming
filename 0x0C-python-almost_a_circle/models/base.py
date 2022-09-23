a3ed37d009c9bd3f51b11fb0f2f18ba2a45c18fa
JamesTeeters <4925@holbertonstudents.com>
2022-09-23 13:43:50 -0500

    'used try to check for filena,e'

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Almost a Circle"""
+from ast import Try
 import json
 
 
@@ -57,11 +58,11 @@ class Base:
         """return a list of all instances of subclasses"""
         filename = cls.__name__ + ".json"
         json_list = []
-        if not (filename):
-            return []
-        else:
+        try:
             with open(filename, 'r') as f:
                 jason_list = cls.from_json_string(f.read())
                 for i, j in enumerate(json_list):
                     json_list[i] = cls.create(**jason_list[i])
                 return jason_list
+        except filename.DoesNotExist:
+            return []
