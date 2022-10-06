a1fc72120fcab2fa665f95f34bd0185abc44fb02
JamesTeeters <4925@holbertonstudents.com>
2022-10-06 13:00:47 -0500

    ''

--- 0x01-SQL_more_queries/1-create_user.sql
+++ 0x01-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- create a User with all permissions
 CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_pwd';
-GRANT ALL ON *.* TO user_0d_1@localhost;
+GRANT ALL PERMISSIONS ON *.* TO user_0d_1@localhost;
