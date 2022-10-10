50fc2427aa4a390fcd58c0f770ebf308be2fc5ab
JamesTeeters <4925@holbertonstudents.com>
2022-10-10 13:26:02 -0500

    ''

--- 0x01-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x01-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,6 @@
 -- now for your favorite shows
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tvshows
-INNER JOIN tv_shows_genres
-ON tv_shows.id = tv_shows_genres.show_id
+FROM tv_shows
+INNER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.genre_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
