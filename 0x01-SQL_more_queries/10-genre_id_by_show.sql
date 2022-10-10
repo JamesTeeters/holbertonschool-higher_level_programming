2240440111e3add5896d8121b061a10caeac1608
JamesTeeters <4925@holbertonstudents.com>
2022-10-10 12:58:08 -0500

    ''

--- 0x01-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x01-SQL_more_queries/10-genre_id_by_show.sql
@@ -2,5 +2,5 @@
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tvshows
 INNER JOIN tv_shows_genres
-on tv_shows.id = tv_shows_genres.show_id
-ORDERED BY tv_shows.title, tv_show_genres.genre_id;
+ON tv_shows.id = tv_shows_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
