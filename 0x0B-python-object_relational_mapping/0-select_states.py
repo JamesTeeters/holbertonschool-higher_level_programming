129a14a8728129c512c127730aad68ee232fea1a
JamesTeeters <4925@holbertonstudents.com>
2022-10-19 06:41:01 -0500

    ''

--- 0x0B-python-object_relational_mapping/0-select_states.py
+++ 0x0B-python-object_relational_mapping/0-select_states.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""list all states in DB"""
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    db_connection = MySQLdb.connect(host="localhost", port=3306,
+                                    usr=sys.argv[1],
+                                    passwrd=sys.argv[2],
+                                    dbname=sys.argv[3])
+    cursor = db_connection.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    db_connection.close()
