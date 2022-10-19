6bf8ba6755a24d14950d51d4fa6be4ca298baf45
JamesTeeters <4925@holbertonstudents.com>
2022-10-19 07:59:05 -0500

    ''

--- 0x0B-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0B-python-object_relational_mapping/4-cities_by_state.py
@@ -9,9 +9,10 @@ if __name__ == '__main__':
                                     passwd=sys.argv[2],
                                     db=sys.argv[3])
     cursor = db_connection.cursor()
-    cursor.execute("SELECT cities.id, cities.name, state.name \
-        FROM cities INNER JOIN states ON cities.state_id=states.id\
-             ORDER BY cities.id ASC")
+    cursor.execute("SELECT cities.'id', cities.'name', state.'name' FROM\
+         'cities' AS cities INNER JOIN 'states' AS\
+             states ON cities.'state_id'=states.'id' \
+            ORDER BY cities.'id' ASC")
     rows = cursor.fetchall()
     for row in rows:
         print(row)
