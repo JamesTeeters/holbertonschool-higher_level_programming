ee60e85ae146623eeb36b5abb781a28bedfb21fc
JamesTeeters <4925@holbertonstudents.com>
2022-10-19 06:50:16 -0500

    ''

--- 0x0B-python-object_relational_mapping/1-filter_states.py
+++ 0x0B-python-object_relational_mapping/1-filter_states.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""list all states starting with the letter N"""
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    db_connection = MySQLdb.connect(host="localhost", port=3306,
+                                    user=sys.argv[1],
+                                    passwd=sys.argv[2],
+                                    db=sys.argv[3])
+    cursor = db_connection.cursor()
+    cursor.execute("SELECT * FROM states WHERE states.name LIKE 'N%' ORDER BY id ASC")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    db_connection.close()
