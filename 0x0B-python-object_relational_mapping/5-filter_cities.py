54692494c433d7e84a7cb63620b71d2a433afe54
JamesTeeters <4925@holbertonstudents.com>
2022-10-19 13:19:31 -0500

    ''

--- 0x0B-python-object_relational_mapping/5-filter_cities.py
+++ 0x0B-python-object_relational_mapping/5-filter_cities.py
@@ -9,9 +9,10 @@ if __name__ == '__main__':
                                     passwd=sys.argv[2],
                                     db=sys.argv[3])
     cursor = db_connection.cursor()
-    cursor.execute("SELECT cities.name FROM cities INNER JOIN states\
-         ON cities.state_id=states.id and states.name=%s",
-                   {sys.arg[4]})
+    cursor.execute("SELECT cities.name FROM cities\
+         INNER JOIN states ON cities.state_id=states.id\
+             and states.name=%s ORDER BY\
+                 cities.id ASC", {sys.arg[4]})
     rows = cursor.fetchall()
     cities = []
     for row in rows:
