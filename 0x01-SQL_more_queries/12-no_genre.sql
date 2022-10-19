e843c146296aa77175a2816e8a8a98d1e399bd5a
JamesTeeters <4925@holbertonstudents.com>
2022-10-18 20:33:34 -0500

    'added all files and README'

--- 0x01-SQL_more_queries/12-no_genre.sql
+++ 0x01-SQL_more_queries/12-no_genre.sql
@@ -1,1 +1,6 @@
 -- stuff
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_genres
+LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.genre_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id
