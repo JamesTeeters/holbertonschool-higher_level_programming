471d7202160527c3dc64886b842adf0175afb415
JamesTeeters <4925@holbertonstudents.com>
2022-10-20 17:00:50 -0500

    ''

--- 0x0B-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0B-python-object_relational_mapping/10-model_state_my_get.py
@@ -14,7 +14,6 @@ if __name__ == '__main__':
     user = sys.argv[1]
     passwrd = sys.argv[2]
     db = sys.argv[3]
-    state_name = sys.arg[4]
 
     db_url = 'mysql+mysqldb://{}:{}@localhost:3306/{}'\
         .format(user, passwrd, db)
@@ -23,7 +22,7 @@ if __name__ == '__main__':
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    states = session.query(State).filter(State.name == state_name).first()
+    states = session.query(State).filter(State.name == sys.arg[4]).first()
     if states:
         print('{}'.format(states.id))
     else:
