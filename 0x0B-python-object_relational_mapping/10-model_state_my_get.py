7c2d9142eda95c631ec7fc2aa16c120e786dc9cd
JamesTeeters <4925@holbertonstudents.com>
2022-10-20 16:51:41 -0500

    ''

--- 0x0B-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0B-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,1 +1,32 @@
 #!/usr/bin/python3
+"""
+print the State Objects
+with name passed as argument using SQLAlchemy
+"""
+from multiprocessing import pool
+from model_state import Base, State
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+if __name__ == '__main__':
+    user = sys.argv[1]
+    passwrd = sys.argv[2]
+    db = sys.argv[3]
+    state_name = sys.arg[4]
+
+    db_url = 'mysql+mysqldb://{}:{}@localhost:3306/{}'\
+        .format(user, passwrd, db)
+
+    engine = create_engine(db_url, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = session.query(State).filter(State.name == state_name).all()
+    if state:
+        print('{}'.format(state.id))
+    else:
+        print("Not found")
+    session.close()
