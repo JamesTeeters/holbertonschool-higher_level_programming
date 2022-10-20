9de35fd18fdc4cc92ee06fe3ec6f4db7b29a3726
JamesTeeters <4925@holbertonstudents.com>
2022-10-20 17:58:07 -0500

    ''

--- 0x0B-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0B-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,1 +1,35 @@
 #!/usr/bin/python3
+"""
+delete State object from database
+using SQLAlchemy
+"""
+from model_state import Base, State
+from model_city import City
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+if __name__ == '__main__':
+    user = sys.argv[1]
+    passwrd = sys.argv[2]
+    db = sys.argv[3]
+
+    db_url = 'mysql+mysqldb://{}:{}@localhost:3306/{}'\
+        .format(user, passwrd, db)
+
+    engine = create_engine(db_url, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    city_list = session.query(State, City).filter\
+        (City.state_id == State.id).order_by\
+            (City.id).all()
+            
+    for states, cities in city_list:
+        print('{}: ({}) {}'.format\
+            (states.name, cities.id, cities.name))
+    session.close()
--- 0x0B-python-object_relational_mapping/model_city.py
+++ 0x0B-python-object_relational_mapping/model_city.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""link table cities to mysql using SQLAlchemy"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+
+class City(Base):
+    """Create class City"""
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
