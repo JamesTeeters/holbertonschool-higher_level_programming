b57746c34fc56610f00bf018c0bfd56b6eb70926
JamesTeeters <4925@holbertonstudents.com>
2022-10-20 17:19:25 -0500

    ''

--- 0x0B-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0B-python-object_relational_mapping/10-model_state_my_get.py
@@ -25,7 +25,7 @@ if __name__ == '__main__':
     session = Session()
     states = session.query(State).filter(State.name == state_name).first()
     if states:
-        print(states.id)
+        print("{}".format(states.id))
     else:
         print("Not found")
     session.close()
