b27678362542488b34d021c6c605cb6972eca634
JamesTeeters <4925@holbertonstudents.com>
2022-10-20 17:37:44 -0500

    ''

--- 0x0B-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0B-python-object_relational_mapping/11-model_state_insert.py
@@ -1,1 +1,29 @@
 #!/usr/bin/python3
+"""
+Adds State object 'Louisiana' to database
+using SQLAlchemy
+"""
+from model_state import Base, State
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+if __name__ == '__main__':
+    user = sys.argv[1]
+    passwrd = sys.argv[2]
+    db = sys.argv[3]
+
+    db_url = 'mysql+mysqldb://{}:{}@localhost:3306/{}'\
+        .format(user, passwrd, db)
+
+    engine = create_engine(db_url, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    added_state = State(name='Louisiana')
+    session.add(added_state)
+    session.commit()
+    print('{}'.format(added_state.id))
+    session.close()
