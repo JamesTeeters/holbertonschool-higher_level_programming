8d1e46eb5c7ed296b5388089ba27ae24ef0cb98c
JamesTeeters <4925@holbertonstudents.com>
2022-10-20 18:14:42 -0500

    ''

--- 0x0B-python-object_relational_mapping/5-filter_cities.py
+++ 0x0B-python-object_relational_mapping/5-filter_cities.py
@@ -10,9 +10,9 @@ if __name__ == '__main__':
                                     db=sys.argv[3])
     cursor = db_connection.cursor()
     cursor.execute("SELECT cities.name FROM cities\
-          JOIN states ON cities.state_id=states.id\
-             WHERE states.name=%s ORDER BY\
-                 cities.id ASC", {sys.arg[4]})
+          JOIN states ON cities.state_id = states.id\
+             WHERE states.name LIKE %s ORDER BY\
+                 cities.id ", {sys.arg[4]})
     rows = cursor.fetchall()
     cities = []
     for row in rows:
