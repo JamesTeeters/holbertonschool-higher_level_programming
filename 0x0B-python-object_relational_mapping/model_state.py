4e0044fde4c11daf42de4eedda938734db988a00
JamesTeeters <4925@holbertonstudents.com>
2022-10-20 13:54:39 -0500

    'first use of SQLAlchemy'

--- 0x0B-python-object_relational_mapping/model_state.py
+++ 0x0B-python-object_relational_mapping/model_state.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""link table states to mysql using SQLAlchemy"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """Create class State"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
