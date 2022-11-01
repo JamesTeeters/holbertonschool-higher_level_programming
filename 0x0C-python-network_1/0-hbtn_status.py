c4da20f8c83429d716b80de3e748c9759a1a92aa
james teeters <teeters@Holby>
2022-11-01 16:01:35 -0500

    typo in Body response

--- 0x0C-python-network_1/0-hbtn_status.py
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -2,11 +2,11 @@
 """Write a Python script that fetches https://intranet.hbtn.io/status"""
 import urllib.request
 
-if __name__ == '__main__':
-    rqst = urllib.request.Request("https://intranet.hbtn.io/status")
-    with urllib.request.urlopen(rqst) as rcved:
-        url = rcved.read()
-        print("Body Response:")
-        print("/t- type: {}".format(type(url)))
-        print("\t- content: {}".format(url))
-        print("\t- utf8 content: {}".format(url.decode("utf-8")))
+
+rqst = urllib.request.Request("https://intranet.hbtn.io/status")
+with urllib.request.urlopen(rqst) as rcved:
+    url = rcved.read()
+    print("Body response:")
+    print("/t- type: {}".format(type(url)))
+    print("\t- content: {}".format(url))
+    print("\t- utf8 content: {}".format(url.decode("utf-8")))
