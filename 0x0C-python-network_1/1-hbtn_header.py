370388493f96bfa6ac98cdb1239968286a969979
james teeters <teeters@Holby>
2022-11-01 16:13:05 -0500

    task 1

---
+++ 0x0C-python-network_1/1-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Write a Python script that takes in a URL,
+sends a request to the URL and displays the value of the X-Request-Id
+variable found in the header of the response.
+"""
+import sys
+import urllib.request
+
+rqst = urllib.request.Request(sys.argv[1])
+with urllib.request.urlopen(rqst) as rcved:
+    print(rcved.headers['X-Request-Id'])
