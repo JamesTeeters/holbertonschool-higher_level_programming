342d9fd32a6fe50df00fb25056b88d7b1bd21882
james teeters <teeters@Holby>
2022-11-01 15:46:58 -0500

    task 0

---
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Write a Python script that fetches https://intranet.hbtn.io/status"""
+import urllib.request
+
+
+rqst = urllib.request.Request("https://intranet.hbtn.io/status")
+with urllib.request.urlopen(rqst) as rcved:
+    url = rcved.read()
+    print("Body Response:")
+    print("/t- type: {}".format(type(url)))
+    print("\t- content: {}".format(url))
+    print("\t- utf8 content: {}".format(url.decode("utf-8")))
