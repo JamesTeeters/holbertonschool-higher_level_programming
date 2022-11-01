e21826eaca41f49f8e85a343e48a6cfc7b17e2c9
james teeters <teeters@Holby>
2022-11-01 17:45:05 -0500

    POST email using request

---
+++ 0x0C-python-network_1/6-post_email.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""POST'email' using request"""
+import sys
+from requests import post
+
+if __name__== '__main__':
+    data = {'email': sys.argv[2]}
+    r = post(sys.argv[1], data)
+    print(r.text)
