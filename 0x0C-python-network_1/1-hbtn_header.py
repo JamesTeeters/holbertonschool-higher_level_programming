20f0e764160b07d3859a86787a442caa2b48508d
james teeters <teeters@Holby>
2022-11-01 16:28:44 -0500

    trying something different

--- 0x0C-python-network_1/1-hbtn_header.py
+++ 0x0C-python-network_1/1-hbtn_header.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """I don't like pep8"""
 import sys
-import urllib.request
+from urllib import request
 
 
 if __name__ == '__main__':
-    rqst = urllib.request.Request(sys.argv[1])
-    with urllib.request.urlopen(rqst) as rcved:
-        print(rcved.headers['X-Request-Id'])
+    with request.urlopen(sys.argv[1]) as f:
+        print(f.headers['X-Request-Id'])
