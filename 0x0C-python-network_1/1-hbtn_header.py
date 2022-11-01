6a77448f0f19df79e517a9f1ddf95883e09df630
james teeters <teeters@Holby>
2022-11-01 16:24:19 -0500

    added import block

--- 0x0C-python-network_1/1-hbtn_header.py
+++ 0x0C-python-network_1/1-hbtn_header.py
@@ -4,6 +4,7 @@ import sys
 import urllib.request
 
 
-rqst = urllib.request.Request(sys.argv[1])
-with urllib.request.urlopen(rqst) as rcved:
-    print(rcved.headers['X-Request-Id'])
+if __name__ == '--main__':
+    rqst = urllib.request.Request(sys.argv[1])
+    with urllib.request.urlopen(rqst) as rcved:
+        print(rcved.headers['X-Request-Id'])
