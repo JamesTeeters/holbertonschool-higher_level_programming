c5e7a142c9e50d227db06e26ea0c96e935e3e345
JamesTeeters <4925@holbertonstudents.com>
2022-12-01 13:57:20 -0800

    4, 5

--- 0x00-javascript-warm_up/2-arguments.js
+++ 0x00-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const num_of_args = process.argv.length;
-if (num_of_args <= 2) {
+const NumOfArgs = process.argv.length;
+if (NumOfArgs <= 2) {
   console.log('No argument');
-} else if (num_of_args === 3) {
+} else if (NumOfArgs === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
