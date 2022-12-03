a85fdbe1bb752193b91e67bf656a0ba63be7f01c
JamesTeeters <4925@holbertonstudents.com>
2022-12-02 16:20:01 -0800

    task 6

--- 0x01-javascript_objects_scopes_closures/6-square.js
+++ 0x01-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+const Rectangle = require('./4-rectangle.js');
+
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charprint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.width; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+};
