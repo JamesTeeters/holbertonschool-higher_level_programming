c0a8f1d51c9313fc4fb6d052e98e60724d96b7f6
JamesTeeters <4925@holbertonstudents.com>
2022-12-06 21:55:59 -0800

    task 5

--- 0x02-javascript-web_scraping/5-request_store.js
+++ 0x02-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,6 @@
 #!/usr/bin/node
+const fs = require('fs');
+const request = require('request');
+const url = process.argv[2];
+const f = process.argv[3];
+request(url).pipe(fs.createWriteStream(f));
