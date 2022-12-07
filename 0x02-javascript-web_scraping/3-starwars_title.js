92a143193fd2d439f8cd50ee49c15bf383765079
JamesTeeters <4925@holbertonstudents.com>
2022-12-06 21:42:13 -0800

    task3

--- 0x02-javascript-web_scraping/3-starwars_title.js
+++ 0x02-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+const request = require('request');
+const movieID = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + movieID;
+request(url, (err, res, body) => {
+  if (err) {
+    console.error(err);
+  } else {
+    const output = JSON.parse(body);
+    console.log(output.title);
+  }
+});
