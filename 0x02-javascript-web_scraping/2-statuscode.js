59fd0bf9dba7a76419417710ece120b3f65740f9
JamesTeeters <4925@holbertonstudents.com>
2022-12-06 21:13:26 -0800

    task 2

--- 0x02-javascript-web_scraping/2-statuscode.js
+++ 0x02-javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+const axios = require('axios');
+const URL = process.argv[2];
+axios.get(URL).then(response => {
+  console.log('code: ' + response.status);
+}).catch(error => {
+  console.log('code: ' + error.response.status);
+});
