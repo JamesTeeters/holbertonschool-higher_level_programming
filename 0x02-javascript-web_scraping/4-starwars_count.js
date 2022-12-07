732b8d5ceb91e97203bea7280b5ec0bdb15a64fd
JamesTeeters <4925@holbertonstudents.com>
2022-12-06 21:51:48 -0800

    task 4

--- 0x02-javascript-web_scraping/4-starwars_count.js
+++ 0x02-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+request(url, (err, res, body) => {
+  if (err) {
+    console.error(err);
+  } else {
+    const output = JSON.parse(body).results;
+    let apperance = 0;
+    for (const key in output) {
+      const characters = output[key].characters;
+      for (const character in characters) {
+        if (characters[character].includes('/18/')) {
+          apperance += 1;
+        }
+      }
+    }
+    console.log(apperance);
+  }
+});
