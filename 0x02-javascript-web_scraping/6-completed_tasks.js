71dee9e12ba5d08ece1a37d892a84af4046c420f
JamesTeeters <4925@holbertonstudents.com>
2022-12-06 22:04:24 -0800

    task 6

--- 0x02-javascript-web_scraping/6-completed_tasks.js
+++ 0x02-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,23 @@
 #!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+request(url, (err, res, body) => {
+  if (err) {
+    console.error(err);
+  } else {
+    const completeTask = {};
+    const content = JSON.parse(body);
+    for (let i = 0; i < content.length; i++) {
+      const id = content[i].userId;
+      const complete = content[i].completed;
+      if (complete) {
+        if (!completeTask[id]) {
+          completeTask[content[i].userId] = 1;
+        } else {
+          completeTask[content[i].userId] += 1;
+        }
+      }
+    }
+    console.log(completeTask);
+  }
+});
